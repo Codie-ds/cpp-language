11c8391a98bad07b785bfbf1885cfc2135165cd8
Codie-ds <121719309+Codie-ds@users.noreply.github.com>
2023-01-01 15:06:16 +0530

    Add files via upload

---
+++ voting.cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,c;
+    cout<<"Enter your age:";
+    cin>>a;
+    
+    if(a>18)
+    {
+    	cout<<"Your are eligible for vout";
+	}
+	else
+	{
+		cout<<"your are not elegible for vout";
+	}
+}
