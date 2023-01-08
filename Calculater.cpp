09a11edec083e72c7f683328910b063195d61d5c
Codie-ds <121719309+Codie-ds@users.noreply.github.com>
2023-01-08 15:04:56 +0530

    Add files via upload

---
+++ Calculater.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    rewrite: 
+	float a,b;
+	char ans;
+
+	cout<<"Enter the number:";
+	cin>>a>>b;
+	
+	cout<<"Enter operation what whould you perform:\n '+' , '-' , '*' , '/':";
+	cin>>ans;
+	
+	switch(ans) // it perform an expression
+	{
+		case '+': cout<<(a+b)<<endl;
+		        break;
+		
+		case '-': cout<<(a-b)<<endl;
+		         break;
+	    case '*': cout<<(a*b)<<endl;
+	             break;
+	    case '/': cout<<(a/b)<<endl;
+	      		 break;
+	     		  
+	    default : cout<<"Invalid"<<endl;
+	            goto rewrite;
+	}
+	return 0;
+}
